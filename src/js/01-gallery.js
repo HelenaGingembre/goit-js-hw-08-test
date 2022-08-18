5e00c4d2d03acadbdcbe0ae485681f15f8b68379
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-08-18 10:45:02 +0300

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from 'simplelightbox';
+
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
@@ -11,19 +14,19 @@ console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
 
 function createGalleryTemplate(galleryItem, index) {
-
-        const template = `<div class="gallery__item">
-        <a class="gallery__link" href="${galleryItem.original}">
+    const { preview, original, description } = galleryItem;
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
             <img
             class="gallery__image"
-            src="${galleryItem.preview}"
-            data-source="${galleryItem.original}"
-            alt="${galleryItem.description}"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
             />
         </a>
         </div>`
 
-    return template;
+    
 } 
 
 function renderAllGallery() {
@@ -33,40 +36,9 @@ function renderAllGallery() {
 
 renderAllGallery();
 
-//2.Реализация делегирования на div.gallery 
 //4. Открытие модального окна по клику на элементе галереи. 
-galleryContainer.addEventListener("click", onOpenModal);
-
-function onOpenModal(event) {
-    
-    // Запрети перенаправление на другую страницу по умолчанию.
-    event.preventDefault();
-    //console.log(event.target.nodeName);
-    if (event.target.nodeName !== "IMG") {
-        console.log('Its not IMG');
-        return;
-    }
-    //2.2 и получение url большого изображения.
-    const imgOriginalUrl = event.target.dataset.source;
-    const description = event.target.alt;
-    // console.log(`ImgOriginalUrl: ${imgOriginalUrl}`);
-    
-    //5. Замена значения атрибута src элемента <img> в модальном окне перед открытием.
-    const instance = basicLightbox.create(`
-        <img src="${imgOriginalUrl}"alt="${description}" />`);
-    instance.show();
-
-    //6.ДОП + Добавь закрытие модального окна по нажатию клавиши Escape. 
-    //Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно.
-    //У библиотеки basicLightbox есть метод для программного закрытия модального окна.
-    window.addEventListener('keydown', onEscapeClick);
-    function onEscapeClick(event) {
-        // console.log(`event.code ==`, event.code);
-        if (event.code == 'Escape') {
-            instance.close();
-            // видалення слухачпод
-            window.removeEventListener('keydown', onEscapeClick);
-        }
-    }
-   
-}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+galleryContainer.addEventListener("click", lightbox);
